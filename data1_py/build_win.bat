859ef4dc14b5674041778ffb50b49bb3eb7c62dd
marcel_hecko <marcel.hecko@moirelabs.com>
2022-03-02 10:49:24 +0100

    add icon

--- data1_py/build_win.bat
+++ data1_py/build_win.bat
@@ -1,2 +1,2 @@
-pyinstaller --onefile --noconsole data1_archive_gui.py && move dist\data1_archive_gui.exe .
-pyinstaller --onefile --noconsole data1_config_gui.py && move dist\data1_config_gui.exe .
+pyinstaller --onefile --noconsole data1_archive_gui.py --icon=m2_icon.ico && move dist\data1_archive_gui.exe .
+pyinstaller --onefile --noconsole data1_config_gui.py --icon=m2_icon.ico && move dist\data1_config_gui.exe .
