66c15fa81dab243f31d9d9f30d45e91c231ad4b2
marcel_hecko <marcel.hecko@moirelabs.com>
2023-05-19 12:23:35 +0200

    added select com port prompt

--- bap2_logger/start.bat
+++ bap2_logger/start.bat
@@ -4,4 +4,7 @@ set "serial_parity=EVEN"
 set "modbus_id=1"
 set "cadence_ms=100"
 
+@echo off
+set /p "serial_port=Select COM port (for example COM3): "
+
 start cmd /k "bap2_logger.exe --port %serial_port% --baudrate %serial_baud% --parity %serial_parity% --cadence_ms %cadence_ms% --modbus_id %modbus_id%"
